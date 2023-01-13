f3e5d60febae63e98f6b8edb6e48dd87999fd381
vishu <bansalvishu56@gmail.com>
2023-01-13 23:13:28 +0530

    Added styles.css

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require("body-parser");
 const app = express();
 
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
