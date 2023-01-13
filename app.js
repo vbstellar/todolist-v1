a0212cbe532a8d0bf9df06bae021bc4aa37f3443
vishu <bansalvishu56@gmail.com>
2023-01-13 15:13:30 +0530

    Made list editable

--- app.js
+++ app.js
@@ -3,40 +3,30 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
+
+var items = ["Buy Food", "Cook Food", "Eat Food"];
 
 app.get("/", function (req, res) {
   var today = new Date();
-  var currentDay = today.getDay();
-  var day = "";
-  switch (currentDay) {
-    case 0:
-      day = "Sunday";
-      break;
-    case 1:
-      day = "Monday";
-      break;
-    case 2:
-      day = "Tuesday";
-      break;
-    case 3:
-      day = "Wednesday";
-      break;
-    case 4:
-      day = "Thursday";
-      break;
-    case 5:
-      day = "Friday";
-      break;
-    case 6:
-      day = "Saturday";
-      break;
-    default:
-      day = "Unexpected Error!!";
-      break;
-  }
 
-  res.render("list", { weekDay: day });
+  var options = {
+    weekday: "long",
+    day: "numeric",
+    month: "long",
+  };
+
+  var day = today.toLocaleDateString("en-US", options);
+
+  res.render("list", { weekDay: day, itemArray: items });
+});
+
+app.post("/", function (req, res) {
+  var item = req.body.newInput;
+  items.push(item);
+  res.redirect("/");
 });
 
 app.listen(3000, () => {
