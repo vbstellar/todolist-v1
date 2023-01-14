a64604e017f34e995af8c5f24c9634793fa942a6
vishu <bansalvishu56@gmail.com>
2023-01-14 13:36:01 +0530

    Date calc in diff file

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
@@ -12,16 +13,7 @@ let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDate();
   res.render("list", { ListTitle: day, itemArray: items });
 });
 
