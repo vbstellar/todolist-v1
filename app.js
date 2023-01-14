e9854b8faf898dad8a12b6b616f4e1ce1f3a194b
vishu <bansalvishu56@gmail.com>
2023-01-14 12:20:29 +0530

    Made layouts + multiple pages

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 let items = ["Buy Food", "Cook Food", "Eat Food"];
+let workItems = [];
 
 app.get("/", function (req, res) {
   let today = new Date();
@@ -21,13 +22,33 @@ app.get("/", function (req, res) {
 
   let day = today.toLocaleDateString("en-US", options);
 
-  res.render("list", { weekDay: day, itemArray: items });
+  res.render("list", { ListTitle: day, itemArray: items });
 });
 
 app.post("/", function (req, res) {
   let item = req.body.newInput;
-  items.push(item);
-  res.redirect("/");
+
+  if (req.body.list === "Work List") {
+    workItems.push(item);
+    res.redirect("/work");
+  } else {
+    items.push(item);
+    res.redirect("/");
+  }
+});
+
+app.get("/work", function (req, res) {
+  res.render("list", { ListTitle: "Work List", itemArray: workItems });
+});
+
+app.post("/work", function (req, res) {
+  let item = req.body.newInput;
+  workItems.push(item);
+  res.redirect("/work");
+});
+
+app.get("/about", function (req, res) {
+  res.render("about");
 });
 
 app.listen(3000, () => {
